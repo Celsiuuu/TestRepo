9ea5d72bc35405dd88d6e295dc36f025d303c561
Celsiuuu <celso.mr@outlook.com>
2025-09-03 11:46:41 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+v print('Hello world!') print('What is your name?') 
+# pergunta o nome
+ w myName = input() 
+ x print('It is good to meet you, ' + myName)
+ y print('The length of your name is:') print(len(myName))
+  z print('What is your age?') 
+ # pergunta a idade 
+ myAge = input()
+  print('You will be ' + str(int(myAge) + 1) + ' in a year.')
